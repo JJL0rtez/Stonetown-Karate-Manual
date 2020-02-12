1a57467fcacb4b5f9c2dc9acf73776d1de11180d
Jaron Lorentz <jjlortez.jl@gmail.com>
2020-02-12 03:50:20 -0500

    Update from Save Data batch

--- Save Files.bat
+++ Save Files.bat
@@ -1,10 +1,3 @@
-echo off
-echo # |-------------------------------------|
-echo # |-------------------------------------|
-echo # |       Stonetown-Karate-Manual       |
-echo # |-------------------------------------|
-echo # |-------------------------------------|
-echo.
 call git status
 call git add .
 call git status
@@ -12,8 +5,3 @@ call git commit -m "Update from Save Data batch"
 call git status
 call git push --all
 call git status
-echo.
-echo # |------------------------------------|
-echo # |                Done                |
-echo # |------------------------------------|
-echo.
--- ff.bat
+++
@@ -1,7 +0,0 @@
-call git status
-call git add .
-call git status
-call git commit -m "Update from Save Data batch"
-call git status
-call git push --all
-call git status
