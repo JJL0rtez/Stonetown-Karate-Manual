6bad6016e973f98a2ecf47bfc40bace136b351e8
Jaron Lorentz <jjlortez.jl@gmail.com>
2020-02-12 03:53:43 -0500

    Update from Save Data batch

--- Update Files.bat
+++ Update Files.bat
@@ -1,12 +1,15 @@
 echo off
-echo "# Stonetown-Karate-Manual UPDATE"
+echo # -------------------------------------
+echo # -------------------------------------
+echo #        Stonetown-Karate-Manual       
+echo # -------------------------------------
+echo # -------------------------------------
 call git status
 call git pull upstream master
 echo.
 echo # --------------------------------------
-echo.
-echo # Done
-echo.
+echo #                 Done
+call git status
 echo # --------------------------------------
 echo.
 @pause
